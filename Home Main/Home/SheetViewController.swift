622e6765065ace54413695cac52df12ba899e4f2
chanakya-bugata <131486569+chanakya-bugata@users.noreply.github.com>
2024-11-20 13:32:28 +0530

    added folder

---
+++ Home Main/Home/SheetViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SheetViewController.swift
+//  Home
+//
+//  Created by admin12 on 18/11/24.
+//
+
+import UIKit
+
+class SheetViewController: UIViewController {
+    
+    
+    @IBOutlet weak var getStartedButton: UIButton!
+    @IBOutlet weak var existingAccountButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func getStartedTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func existingAccountTapped(_ sender: UIButton) {
+    }
+}
